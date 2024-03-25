0f25a43d5893e16688edf0fee1844cdd72c9d54a
Nick Santos <nick.santos@docker.com>
2024-03-24 20:09:33 -0400

    use swaylock-effects
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -euo pipefail;
-
-PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-
-rm -f ~/.config/sway/locksreen.png;
-grim ~/.config/sway/lockscreen.png;
-convert -filter Gaussian -resize 20% -blur 0x2.5 -resize 500% ~/.config/sway/lockscreen.png ~/.config/sway/lockscreen.png;
-swaylock -e -f -i ~/.config/sway/lockscreen.png;
-
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color bb00cc \
+	--key-hl-color 880033 \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--grace 10 \
+	--fade-in 0.2;
