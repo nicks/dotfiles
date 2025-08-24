a6536e09b5a02382cec1634d8605a876d5def548
Nick Santos <nick.santos@docker.com>
2025-08-23 21:23:11 -0400

    keyboard tweaks
    - add claude code to emacs
    - add udev rules to disable mouse
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- bin/keyboard-internal-disable.sh
+++ bin/keyboard-internal-disable.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo 1 > /sys/devices/platform/i8042/serio0/input/input3/inhibited
+echo 1 > /sys/devices/pci0000:00/0000:00:15.1/i2c_designware.1/i2c-1/i2c-SYNA8004:00/0018:06CB:CD8B.0001/input/input9/inhibited
+echo 1 > /sys/devices/pci0000:00/0000:00:15.1/i2c_designware.1/i2c-1/i2c-SYNA8004:00/0018:06CB:CD8B.0001/input/input10/inhibited
