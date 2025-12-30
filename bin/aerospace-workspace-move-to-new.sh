56d1c45dc77c3af65f1fccbd2e2520b665bca579
Nick Santos <nicholas.j.santos@gmail.com>
2025-12-30 18:19:53 -0500

    tweak aerospace workspaces in sketchybar (#51)

--- bin/aerospace-workspace-move-to-new.sh
+++ bin/aerospace-workspace-move-to-new.sh
@@ -5,5 +5,6 @@ for sid in $(aerospace list-workspaces --all); do
     count=$(aerospace list-windows --workspace $sid | wc -l | tr -d ' ')
     if [[ "$focused" != "$sid" && "$count" == "0" ]]; then
         aerospace move-node-to-workspace --focus-follows-window "$sid"
+        exit 0
     fi
 done
--- sketchybar/plugins/aerospace_item.sh
+++ sketchybar/plugins/aerospace_item.sh
@@ -6,6 +6,10 @@ previous="aerospace"
 for sid in $(aerospace list-workspaces --all); do
     count=$(aerospace list-windows --workspace $sid | wc -l | tr -d ' ')
     if [[ "$focused" == "$sid" || "$count" != "0" ]]; then
+        color=0xffffffff
+        if [[ "$focused" == "$sid" ]]; then
+            color=0xff66ff66
+        fi
         sketchybar --add item space.$sid left \
                    --set space.$sid \
                    background.color=0x44ffffff \
@@ -13,6 +17,7 @@ for sid in $(aerospace list-workspaces --all); do
                    background.height=20 \
                    background.drawing=off \
                    label="$sid" \
+                   label.color=$color \
                    click_script="aerospace workspace $sid" \
                    script="$CONFIG_DIR/plugins/aerospace.sh $sid"
         sketchybar --move space.$sid after "$previous"
