3b15bd08fbb747f601f2d2d5ab72b4646f47c8cf
Nick Santos <nick.santos@docker.com>
2026-04-13 13:09:50 -0400

    improve multi-monitor support an aerospace
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- bin/aerospace-workspace-move-to-new.sh
+++ bin/aerospace-workspace-move-to-new.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+focused_monitor=$(aerospace list-monitors --focused | awk '{print $1}')
 focused=$(aerospace list-workspaces --focused | tr -d '\n')
 for sid in $(aerospace list-workspaces --all); do
     count=$(aerospace list-windows --workspace $sid | wc -l | tr -d ' ')
     if [[ "$focused" != "$sid" && "$count" == "0" ]]; then
         aerospace move-node-to-workspace --focus-follows-window "$sid"
+        aerospace move-workspace-to-monitor "$focused_monitor"
         exit 0
     fi
 done
--- bin/aerospace-workspace-new.sh
+++ bin/aerospace-workspace-new.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+focused_monitor=$(aerospace list-monitors --focused | awk '{print $1}')
 focused=$(aerospace list-workspaces --focused | tr -d '\n')
-previous="aerospace"
 for sid in $(aerospace list-workspaces --all); do
     count=$(aerospace list-windows --workspace $sid | wc -l | tr -d ' ')
     if [[ "$focused" != "$sid" && "$count" == "0" ]]; then
         aerospace workspace "$sid"
+        aerospace move-workspace-to-monitor "$focused_monitor"
         exit 0
     fi
 done
